6653d1c892a698e2c86b79635e714ae399c4c68e
k9606 <bishengfei@126.com>
2019-07-25 10:01:07 +0800

    添加忽略

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     return 'Welcome to My Watchlist!'
+
+
+# ...
+@app.route('/ss')
+def index():
+    return render_template('index.html')
